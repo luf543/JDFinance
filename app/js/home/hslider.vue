016ca7e79db3c05a31bea97c0f7c88b807984e9f
lufeng.tuniu <lufeng@tuniu.com>
2018-03-07 20:11:32 +0800

    5-11

--- app/js/home/hslider.vue
+++ app/js/home/hslider.vue
@@ -45,6 +45,10 @@ export default {
         title: "抢钢镚",
       },{
         href: "home",
+        img: "//img12.360buyimg.com/jrpmobile/jfs/t4618/115/2179710654/11347/bf520df/58ec9d84Nfd169001.png?width=132&height=132",
+        title: "领京豆",
+      },{
+        href: "home",
         img: "//img12.360buyimg.com/jrpmobile/jfs/t5488/298/1036263348/12073/b4f4de97/590ac8e8Ne9def22e.png?width=135&height=135",
         title: "白条提额",
       }],
