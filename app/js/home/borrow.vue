76b28ee3468d9433ab7be9703c17bbd92864089a
lufeng.tuniu <lufeng@tuniu.com>
2018-03-08 19:25:19 +0800

    5-15完结

--- app/js/home/borrow.vue
+++ app/js/home/borrow.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <Panel title="极速借贷" :class="$style.panel">
+    <Panel title="极速借贷">
       <section :class="$style.content">
         <router-link :to="{ name: 'home' }">
           <img src="//img12.360buyimg.com/jrpmobile/jfs/t12721/42/2497849749/26333/ec584be4/5a421756N2416c88f.png?width=750&height=280" alt="">
@@ -18,13 +18,11 @@ export default {
 </script>
 
 <style lang="scss" module>
-.panel{
-  .content{
-    padding-bottom: 40px;
-    img{
-      display: block;
-      width: 100%;
-    }
+.content{
+  padding-bottom: 40px;
+  img{
+    display: block;
+    width: 100%;
   }
 }
 </style>
--- app/js/home/money.vue
+++ app/js/home/money.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <Panel title="理财精选" :class="$style.panel">
+    <Panel title="理财精选">
       <section :class="$style.content">
         <dl :class="$style.item" v-for="item in items" :key="item.title">
           <dt>{{ item.title }}<span>{{ item.sub }}</span></dt>
@@ -45,5 +45,76 @@ export default {
 </script>
 
 <style lang="scss" module>
-
+  @import '../../css/element.scss';
+  .content{
+    @include flex(row);
+    justify-content: space-around;
+    box-sizing: border-box;
+    &:after{
+      content: " ";
+      display: block;
+      width: 100%;
+      height: 0;
+      box-sizing: border-box;
+      border-bottom: 1px solid #ddd;
+      position: relative;
+      top: -208px;
+    }
+    .item{
+      position: relative;
+      width: 50%;
+      box-sizing: border-box;
+      &:after{
+        content: " ";
+        width: 0;
+        height: 136px;
+        display: block;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        margin-top: -68px;
+        border-right: 1px solid #eee;
+      }
+      &:nth-child(2n){
+        &:after{
+          display: none;
+        }
+      }
+      padding: 34px 16px;
+      dt{
+        font-size: 30px;
+        line-height: 42px;
+        color: #333;
+        span{
+          font-size:22px;
+          color:#ff5155;
+          border: 1px solid #ff5155;
+          padding: 0 8px;
+          vertical-align: 1px;
+          margin-left: 2px;
+        }
+      }
+      dd{
+        &:nth-child(2){
+          font-weight: 700;
+          font-size: 44px;
+          height: 58px;
+          line-height: 58px;
+          color: #FF5155;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+        &:nth-child(3){
+          font-size: 24px;
+          height: 34px;
+          line-height: 34px;
+          color: #999;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+      }
+    }
+  }
 </style>
